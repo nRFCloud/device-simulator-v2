cb2c612d8e3b290266383a383884e03cb467cb52
Patrick Barnes <patrick@devinsites.com>
2019-10-04 20:47:06 -0700

    wip: initial commit

--- src/simulator.ts
+++ src/simulator.ts
@@ -1,6 +1,4 @@
-import * as program from 'commander';
-import { cyan, red, yellow } from 'colors';
-import * as dotenv from 'dotenv';
+import { cyan, yellow } from 'colors';
 import * as path from 'path';
 import { nrfdevice, DeviceConfig } from './nrfDevice';
 import { ISensor } from './sensors/Sensor';
@@ -8,17 +6,40 @@ import { FakeGps } from './sensors/FakeGps';
 import { FakeAccelerometer } from './sensors/FakeAccelerometer';
 import { FakeThermometer } from './sensors/FakeThermometer';
 import { FakeDevice } from './sensors/FakeDevice';
+import { device } from 'aws-iot-device-sdk';
 
-dotenv.config();
+export type SimulatorConfig = {
+  certsResponse: string;
+  endpoint: string;
+  appFwVersion: string;
+  deviceId: string;
+  mqttMessagesPrefix: string;
+  services?: string;
+  apiKey?: string;
+  apiHost?: string;
+  deviceOwnershipCode?: string;
+  verbose?: boolean;
+  associate?: boolean;
+  onConnect?: (deviceId: string, client: device) => void;
+};
 
-const simulator = async ({
+export const simulator = async ({
   certsResponse,
   endpoint,
   appFwVersion,
   mqttMessagesPrefix,
-  services,
-}: program.Command) => {
-  const certs = JSON.parse(certsResponse);
+  services = '',
+  onConnect,
+}: SimulatorConfig): Promise<void> => {
+  let certs;
+
+  try {
+    certs = JSON.parse(certsResponse);
+  } catch (err) {
+    console.log('certsReponse', certsResponse);
+    throw new Error(`Error parsing certsResponse ${err} ${certsResponse}`);
+  }
+
   const caCert = Buffer.from(certs.caCert, 'utf-8');
   const clientCert = Buffer.from(certs.clientCert, 'utf-8');
   const privateKey = Buffer.from(certs.privateKey, 'utf-8');
@@ -36,6 +57,7 @@ const simulator = async ({
   console.log(cyan(`connecting to ${yellow(endpoint)}...`));
 
   const sensors = new Map<string, ISensor>();
+
   if (services) {
     services.split(',').map((service: string) => {
       const sensorDataFilePath = (filename: string) =>
@@ -78,36 +100,5 @@ const simulator = async ({
     });
   }
 
-  nrfdevice(config, sensors);
+  nrfdevice(config, sensors, onConnect);
 };
-
-program
-  .option(
-    '-cr, --certs-response <certsResponse>',
-    'JSON returned by call to the Device API endpoint: POST /devices/{deviceid}/certificates',
-    process.env.CERTS_RESPONSE,
-  )
-  .option(
-    '-e, --endpoint <endpoint>',
-    'AWS IoT MQTT endpoint',
-    process.env.MQTT_ENDPOINT,
-  )
-  .option(
-    '-a, --app-fw-version <appFwVersion>',
-    'Version of the app firmware',
-    1,
-  )
-  .option(
-    '-m, --mqtt-messages-prefix <mqttMessagesPrefix>',
-    'The prefix for the MQTT unique to this tenant for sending and receiving device messages',
-    process.env.MQTT_MESSAGES_PREFIX,
-  )
-  .option(
-    '-s, --services <services>',
-    'Comma-delimited list of services to enable. Any of: [gps,acc,temp,device]',
-  )
-  .parse(process.argv);
-
-simulator(program).catch(error => {
-  process.stderr.write(`${red(error)}\n`);
-});
