5c72dc9afb3f8d4791b48562527c58420832959e
sherretten <nathaniels2016@gmail.com>
2024-07-30 17:29:25 -0700

    almost have the key

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { device } from 'aws-iot-device-sdk';
 import { AxiosInstance } from 'axios/index';
 import axios from 'axios';
+import { execSync } from 'child_process';
 
 import { simulator } from './simulator';
 import { Log } from './models/Log';
@@ -73,6 +74,19 @@ export const associateDevice = ({
     deviceOwnershipCode,
   );
 
+export const onboardDevice = ({
+	deviceId,
+	certsResponse,
+	apiHost,
+	apiKey,
+	verbose,
+}: Partial<SimulatorConfig>) => {
+	const certificate = JSON.parse(certsResponse as string).certificate;
+	return getConn(apiHost as string, apiKey as string, !!verbose).post(
+	`v1/devices/${deviceId}`, certificate
+)
+}
+
 export const getDefaults = async ({
   deviceId,
   endpoint,
@@ -126,14 +140,34 @@ export const getDefaults = async ({
     let defaultJsonCert = cachedDefaults.certsResponse || '';
 
     // NATE: Add code here
-    if (!defaultJsonCert && associate !== 'jitp') {
-      log.debug('Fetching cert from device API.\n');
-      const { data } = await conn.post(
-        `/v1/devices/${deviceId}/certificates`,
-        deviceOwnershipCode,
-      );
+    if (!defaultJsonCert) {
+			if (associate === 'jitp') {
+				log.debug('Fetching cert from device API.\n');
+				const { data } = await conn.post(
+					`/v1/devices/${deviceId}/certificates`,
+					deviceOwnershipCode,
+				);
+	
+				defaultJsonCert = JSON.stringify(data);
+			} else {
+				log.debug('Generating self signed device certs.\n');
+				const privateKey = execSync(`openssl ecparam -name prime256v1 -genkey`);
 
-      defaultJsonCert = JSON.stringify(data);
+				const subject = "/C=NO/ST=Norway/L=Trondheim/O=Nordic Semiconductor/OU=Test Devices"
+				const caCert = execSync(`echo "${privateKey}" | openssl req -x509 -extensions v3_ca -new -nodes -key /dev/stdin -sha256 -days 1024 -subj "${subject}"`);
+				console.debug('CA Cert', caCert);
+
+				let deviceCSR = execSync(`openssl ecparam -name prime256v1 -genkey`);
+				console.debug('ECC', deviceCSR);
+				deviceCSR = execSync(`echo "${deviceCSR}" | openssl pkcs8 -topk8 -nocrypt -in /dev/stdin`);
+				console.debug('Device PEM', deviceCSR);
+				deviceCSR = execSync(`echo "${deviceCSR}" | openssl req -new -key /dev/stdin -subj "${subject}/CN=${deviceId}"`);
+				console.debug('Device CSR', deviceCSR);
+				deviceCSR = execSync(`openssl x509 -req -in <(echo "${deviceCSR}") -CA <(echo "${caCert}") -CAkey <(echo"${privateKey}") -CAcreateserial -days 10950 -sha256`)
+				console.debug('Device Cert', deviceCSR);
+
+				defaultJsonCert = JSON.stringify({privateKey, caCert, certificate: deviceCSR})
+			}
     }
 
     defaults.certsResponse = defaultJsonCert;
@@ -225,20 +259,24 @@ export const run = async (config: SimulatorConfig): Promise<void> => {
   if (associate) {
     config.onConnect = async (deviceId) => {
       log.info(
-        `ATTEMPTING TO ASSOCIATE ${config.deviceId} WITH API KEY ${config.apiKey} VIA ${config.apiHost}`,
+        `ATTEMPTING TO ${associate === 'jitp'? 'ASSOCIATE': 'ONBOARD'} ${config.deviceId} WITH API KEY ${config.apiKey} VIA ${config.apiHost}`,
       );
 
       // wait to ensure the device is available in AWS IoT so it can be associated
       await new Promise((resolve) => setTimeout(resolve, 2000));
 
       try {
-        await associateDevice({
-          deviceId,
-          deviceOwnershipCode,
-          apiHost,
-          apiKey,
-          verbose,
-        });
+				if (associate === 'jitp') {
+					await associateDevice({
+						deviceId,
+						deviceOwnershipCode,
+						apiHost,
+						apiKey,
+						verbose,
+					});
+				} else {
+					await onboardDevice({deviceId, apiHost, apiKey, certsResponse, verbose})
+				}
 
         log.success('DEVICE ASSOCIATED!');
       } catch (err) {
